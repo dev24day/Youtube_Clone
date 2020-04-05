5defd2c7272940321e4c12b7f5531b489d91fd66
PPPPPKyung27 <kjyanus@gmail.com>
2020-04-05 16:36:45 +0900

    2.3FirstExpressServer

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hi");
+const express = require("express");
+const app = express();
+const PORT = 4000;
+
+function handleListen(){
+    console.log(`Listening on: localhost:${PORT}`);
+}
+app.listen(4000, handleListen);
