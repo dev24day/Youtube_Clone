21a1da4090819655adb4c137114f0609b5a71287
Guest <guest-4opjkr@h23559.sgb.skir.kr>
2020-04-06 21:12:17 +0900

    2.4 Handling Route

--- index.js
+++ index.js
@@ -2,7 +2,22 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 
-function handleListen(){
-    console.log(`Listening on: localhost:${PORT}`);
+function handleListen() {
+  console.log(`Listening on: localhost:${PORT}`);
 }
+
+function handleHome(req, res) {
+  res.send("Respond success");
+  console.log("Hi from home");
+}
+function handlePlus(req, res) {
+  res.send("Success again");
+  console.log("Hi plus");
+}
+app.get("/", handleHome);
+app.get("/plus", handlePlus);
+// if someone tries to "get" to "/"
+// After 'get' request -> should be 'respond' request
+// respond can be done by HTML, console, error, ...
+// otherwise, loading forever..
 app.listen(4000, handleListen);
