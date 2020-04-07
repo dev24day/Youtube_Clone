899724c21a1b184b36deb48be0bbab9a68daf296
Guest <guest-llwbp9@h26753.sgb.skir.kr>
2020-04-07 20:53:52 +0900

    2.6 Middleware part One

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ const PORT = 4000;
 const handleListen = () => console.log(`Listening on: localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send("Respond success");
-
 const handlePlus = (req, res) => res.send("Success again");
 
+const handleMiddle = (req, res, next) => {
+  console.log("I am Middleware");
+  next();
+};
+
+app.use(handleMiddle);
+//Put as much as Middlewares with app.use()
 app.get("/", handleHome);
 app.get("/plus", handlePlus);
-// if someone tries to "get" to "/"
-// After 'get' request -> should be 'respond' request
-// respond can be done by HTML, console, error, ...
-// otherwise, loading forever..
+
 app.listen(4000, handleListen);
