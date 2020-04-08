bbe0ab27f70a4629972129b2f2e18e52b37fb849
PPPPPKyung27 <kjyanus@gmail.com>
2020-04-08 11:20:52 +0900

    2.7 MiddleWare part Two

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
 const app = express();
 const PORT = 4000;
 
@@ -7,13 +12,15 @@ const handleListen = () => console.log(`Listening on: localhost:${PORT}`);
 const handleHome = (req, res) => res.send("Respond success");
 const handlePlus = (req, res) => res.send("Success again");
 
-const handleMiddle = (req, res, next) => {
-  console.log("I am Middleware");
-  next();
-};
+//middleware에서 res.send를 실행하면 연결을 끊어버림
+const middleWare = (req, res, next) => res.send("Not Happening");
 
-app.use(handleMiddle);
-//Put as much as Middlewares with app.use()
+app.use(cookieParser()); // understand cookie from user to use session
+app.use(bodyParser.json()); //access to request obj that has form data
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
 app.get("/", handleHome);
 app.get("/plus", handlePlus);
 
