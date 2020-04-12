951daadf1bae79d7821e9c5148c94a3c45f76f81
PPPPPKyung27 <kjyanus@gmail.com>
2020-04-12 11:56:56 +0900

    2.16 Partials & Local variables

--- app.js
+++ app.js
@@ -7,17 +7,19 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 
+app.use(helmet());
 app.set("view engine", "pug"); //set the view engine (default: undefined) to pug
 //app.set("views", "news") // change the path
 app.use(cookieParser()); // understand cookie from user to use session
 app.use(bodyParser.json()); //access to request obj that has form data
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
 
+app.use(localsMiddleware);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
 app.use(routes.home, globalRouter);
