6509fa083c82c94b4f7043402dafca8916857ef8
PPPPPKyung27 <kjyanus@gmail.com>
2020-04-12 15:16:45 +0900

    2.17 Template Variables

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,8 @@
-/*export const join = (req, res) => res.send("Join");
-export const login = (req, res) => res.send("Login");
-export const logout = (req, res) => res.send("Logout");
-export const users = (req, res) => res.send("users");
-export const user_detail = (req, res) => res.send("user_detail");
-export const edit_profile = (req, res) => res.send("edit_profile");
-export const change_password = (req, res) => res.send("change_password");
-*/
-export const home = (req, res) => res.render("home");
-export const join = (req, res) => res.render("join");
-//find template file name 'home.pug' from folder '/views' since I set view engine 'pug'
-export const login = (req, res) => res.render("login");
-export const logout = (req, res) => res.render("logout");
-export const users = (req, res) => res.render("users");
-export const user_detail = (req, res) => res.render("user_detail");
-export const edit_profile = (req, res) => res.render("edit_profile");
-export const change_password = (req, res) => res.render("change_password");
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+export const login = (req, res) => res.render("login", { pageTitle: "Login" });
+export const logout = (req, res) => res.render("logout", { pageTitle: "Logout" });
+export const users = (req, res) => res.render("users", { pageTitle: "Users" });
+export const user_detail = (req, res) => res.render("user_detail", { pageTitle: "User_detail" });
+export const edit_profile = (req, res) => res.render("edit_profile", { pageTitle: "Edit_profile" });
+export const change_password = (req, res) => res.render("change_password", { pageTitle: "Change_password" });
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,6 @@
-export const search = (req, res) => res.render("search");
-export const videos = (req, res) => res.render("videos");
-export const upload = (req, res) => res.render("upload");
-export const video_detail = (req, res) => res.render("video_detail");
-export const edit_video = (req, res) => res.render("edit_video");
-export const delete_video = (req, res) => res.render("delete_video");
+export const search = (req, res) => res.render("search", { pageTitle: "Search" });
+export const videos = (req, res) => res.render("videos", { pageTitle: "Videos" });
+export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
+export const video_detail = (req, res) => res.render("video_detail", { pageTitle: "Video_detail" });
+export const edit_video = (req, res) => res.render("edit_video", { pageTitle: "Edit_video" });
+export const delete_video = (req, res) => res.render("delete_video", { pageTitle: "Delete_video" });
